1a6af24888272415552df51819d03c681edd8097
Risto Vatsar <r.vatsar@gmail.com>
2023-02-12 15:50:39 +0200

    first commit

---
+++ Week16BitcoinApp/BitCoinRate.cs
@@ -0,0 +1,30 @@
+﻿namespace Week16BitcoinApp
+{
+    public class BitCoinRate
+    {
+        public Disclaimer disclaimer { get; set; }
+        public Bpi bpi { get; set; }
+    }
+
+    public class Disclaimer
+    {
+        public string disclaimer { get; set; }
+    }
+
+    public class Bpi
+    {
+        public EUR EUR { get; set; }
+        public USD USD { get; set; }
+    }
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+    public class USD
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+}
---
+++ Week16BitcoinApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using Week16BitcoinApp;
+
+BitCoinRate bitcoin = GetRates();
+Console.WriteLine($"Current Bitcoin rate is: {bitcoin.bpi.EUR.rate_float} {bitcoin.bpi.EUR.code}");
+Console.WriteLine($"Current Bitcoin rate is: {bitcoin.bpi.USD.rate_float} {bitcoin.bpi.USD.code}");
+
+//Console.WriteLine($"{bitcoin.disclaimer}");
+
+static BitCoinRate GetRates()
+{
+    string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+    request.Method = "GET";
+
+    var webResponse = request.GetResponse();
+    var webStream = webResponse.GetResponseStream();
+
+    BitCoinRate bitCoinRate;
+
+    using (var responseReader = new StreamReader(webStream))
+    {
+        var response = responseReader.ReadToEnd();
+        bitCoinRate = JsonConvert.DeserializeObject<BitCoinRate>(response);
+    }
+    return bitCoinRate;
+}
